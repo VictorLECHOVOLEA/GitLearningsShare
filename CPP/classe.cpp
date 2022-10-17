bcd93a220ff1bbb85f5564e4d208645f7568dca9
VictorLECHOVOLEA <lechovoleavictor@gmail.com>
2022-10-17 22:25:48 +0300

    My first commit

---
+++ CPP/classe.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BIMPlayer {
+private:
+    int age;
+    string name;
+    string country;
+
+public:
+    
+    BIMPlayer player;
+
+};
+
+int main() {
+    return0;
+}
+
+
+// how to install git
+// https://youtu.be/i_23KUAEtUM
