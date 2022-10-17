240b5e9df3762ccd89d553b4179abbc6d5afe45e
VictorLECHOVOLEA <lechovoleavictor@gmail.com>
2022-10-17 22:37:03 +0300

    added features

--- CPP/classe.cpp
+++ CPP/classe.cpp
@@ -7,17 +7,34 @@ private:
     int age;
     string name;
     string country;
+    char team;
 
 public:
     
-    BIMPlayer player;
+    BIMPlayer(int a, string n, string c, char t) {
+        age = a;
+        name = n;
+        country = c;
+        team = t;
+    }
+    void setAge(int a) {
+        age = a;
+    }
 
 };
 
 int main() {
+    int age;
+    cin >> age;
+    string name;
+    cin >> name;
+    string country;
+    cin >> country;
+    char team;
+    cin >> team;
+
     return0;
 }
-
 
 // how to install git
 // https://youtu.be/i_23KUAEtUM
