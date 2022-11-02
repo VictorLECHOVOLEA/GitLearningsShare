c3315e41bc8c099ba61a0441ae4029d12064fb27
VictorLECHOVOLEA <lechovoleavictor@gmail.com>
2022-11-02 23:04:34 +0200

    Added my first example from Chapter 8

--- CPP/Learning/VLE_CPP_classe.cpp
+++ CPP/Learning/VLE_CPP_classe.cpp
@@ -1,3 +1,53 @@
+/*
+Author: Victor Lechovolea
+https://github.com/VictorLECHOVOLEA
+Briefly, everything I learned about C++ classe
+*/
+
+
+
+
+// /* 
+// Example 002 -- START 
+#include <iostream>
+using namespace std;
+
+// by convention, class is defined with class keyword
+// by convention, Class objects start with Uppercase letter.
+class Critter {     // class definition --defines a new type Critter
+public:
+    int m_Hunger;   // data member
+    void Greet();   // member function prototype
+}; // class is surrounded by curly brackets and end with a semicolon.
+
+// function definition outside class constructor. :: operator
+void Critter::Greet() { // member function definition
+    cout << "Hi. I'm a critter. My hunger level is " << m_Hunger << ".\n";
+}
+
+int main() {
+    Critter crit1;
+    Critter crit2;
+    crit1.m_Hunger = 9;
+    cout << "crit1's hunger level is " << crit1.m_Hunger << ".\n";
+
+    crit2.m_Hunger = 3;
+    cout << "crit2's hunger level is " << crit2.m_Hunger << ".\n";
+
+    crit1.Greet();
+    crit2.Greet();
+    return 0;
+}
+
+// Example -- END
+// */
+
+
+
+
+/* 
+// Example 000 
+// Example -- START 
 #include <iostream>
 #include <string>
 using namespace std;
@@ -33,8 +83,11 @@ int main() {
     char team;
     cin >> team;
 
-    return0;
+    return 0;
 }
 
 // how to install git
 // https://youtu.be/i_23KUAEtUM
+
+// Example -- END
+// */
